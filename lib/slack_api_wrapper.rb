8655cedee05bf156dc1e3d13b60b6cf091ec90af
beccaelenzil <beccaelenzil@gmail.com>
2019-07-12 18:52:13 -0700

    completed slack post exercise

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,10 +1,27 @@
 # slack_api_wrapper.rb
 require "httparty"
+require "dotenv"
+Dotenv.load
 
 module SlackApi
-  BASE_URL = "https://slack.com/api/"
-  API_KEY = ENV["SLACK_TOKEN"]
+  BASE_URL = 'https://slack.com/api/'
+  API_KEY = ENV['SLACK_TOKEN']
 
-  # Code goes here
+  def self.send_msg(message, channel)
 
+    response = HTTParty.post(
+      "#{BASE_URL}/chat.postMessage",
+      body:  {
+        token: API_KEY,
+        text: message,
+        channel: channel
+      },
+      headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    )
+    puts "response #{response.parsed_response}" 
+    puts "ENV: #{ENV.keys}"
+    puts "API Token: #{API_KEY}"
+
+    return response.code == 200 && response.parsed_response["ok"]
+  end
 end
--- specs/slack_api_wrapper_spec.rb
+++ specs/slack_api_wrapper_spec.rb
@@ -1,6 +1,12 @@
-# slack_api_wrapper_spac.rb
+# slack_api_wrapper_spec.rb
 require_relative 'spec_helper'
 
-describe SlackApiWrapper do
-
+describe SlackApi do
+  it "can send a valid message" do
+    VCR.use_cassette("slack-posts") do
+      response = SlackApi.send_msg("Hey I can post messages!", "testing")
+      expect(response).must_equal true
+    end
+  end
 end
+
